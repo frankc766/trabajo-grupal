5208478c232e8a55790d9d3aa4722ebd66341c11
frankc766 <85146275+frankc766@users.noreply.github.com>
2021-07-16 00:09:33 -0600

    agrege los codigos a la clase que contiene el metodo principal

--- src/grupo6/Grupo6.java
+++ src/grupo6/Grupo6.java
@@ -1,6 +1,7 @@
 
 package grupo6;
-
+import helpers.Deportista;
+import helpers.Doctor;
 /**
  *
  * @author ( grupo6)
@@ -11,7 +12,27 @@ public class Grupo6 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Deportista persona1= new Deportista();
+       Doctor persona2=new Doctor();
+       
+        System.out.println("***********************************************");
+        persona1.setnombre("Josue Gomez" );
+        persona1.setedad(21);
+        persona1.setprofesion("Deportista");
+        
+        
+        System.out.println("mi nombre es "+persona1.getnombre());
+        System.out.println("tengo "+persona1.getedad()+" años");
+        System.out.println("mi profesion es ingeniero pero tambien soy "+persona1.getprofesion());
+        
+        System.out.println("**********************************************");
+        persona2.setnombre("Haniel Perez" );
+        persona2.setedad(33);
+        persona2.setprofesion("doctor");
+        System.out.println(persona2.getnombre());
+        System.out.println("tengo "+persona2.getedad()+" años");
+        System.out.println(persona2.getprofesion());
+        System.out.println("**********************************************");
     }
     
 }
