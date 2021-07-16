725f32873e797b9a159c8fd286d2dced6a00dc93
frankc766 <85146275+frankc766@users.noreply.github.com>
2021-07-15 22:23:56 -0600

    comenzando

---
+++ src/grupo6/Grupo6.java
@@ -0,0 +1,17 @@
+
+package grupo6;
+
+/**
+ *
+ * @author ( grupo6)
+ */
+public class Grupo6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
