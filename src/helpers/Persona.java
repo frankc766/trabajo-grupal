8a1c7f74364ed260d74c58125417ac062190c79a
frankc766 <85146275+frankc766@users.noreply.github.com>
2021-07-15 22:55:36 -0600

    creacion de clase padre o superclase

---
+++ src/helpers/Persona.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helpers;
+
+/**
+ *
+ * @author (juan cruz)
+ */
+    public abstract class Persona {
+    //metodos abstractos a heredar
+  public abstract String getnombre();
+    public abstract int getedad();
+    public abstract String getprofesion();
+    
+}
